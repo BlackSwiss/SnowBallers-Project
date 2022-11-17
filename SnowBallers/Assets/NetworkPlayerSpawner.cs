b30d0c1aaf99b8c400cea79863654e461905057f
BlackSwiss <47673343+BlackSwiss@users.noreply.github.com>
2022-11-16 19:27:08 -0800

    Added vr package hopefully no bugs

---
+++ SnowBallers/Assets/NetworkPlayerSpawner.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkPlayerSpawner : MonoBehaviourPunCallbacks
+{
+
+    private GameObject spawnedPlayerPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        spawnedPlayerPrefab = PhotonNetwork.Instantiate("Network Player", transform.position, transform.rotation);
+    }
+
+    public override void OnLeftRoom()
+    {
+        base.OnLeftRoom();
+        PhotonNetwork.Destroy(spawnedPlayerPrefab);
+    }
+}
