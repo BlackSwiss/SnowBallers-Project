1832716417133726b31019f5652734fe302aa52c
BlackSwiss <47673343+BlackSwiss@users.noreply.github.com>
2022-12-02 14:52:12 -0800

    Added my changes to the current lobby and vr test 2

--- SnowBallers/Assets/Scripts/HMDInfoManager.cs
+++ SnowBallers/Assets/Scripts/HMDInfoManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.XR;
 public class HMDInfoManager : MonoBehaviour
 {
     // Start is called before the first frame update
+    //TURN THIS OBJECT ON IF YOU WANT KEYBOARD CONTORLS IN THIS SCENE
+    public GameObject HMDSimulator;
     void Start()
     {
         Debug.Log("Is device active: " + XRSettings.isDeviceActive);
@@ -28,6 +30,15 @@ public class HMDInfoManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            Debug.Log("Turned on Keyboard Controls!");
+            turnonKeyboardControls();
+        }
+    }
+
+    void turnonKeyboardControls()
+    {
+        HMDSimulator.SetActive(true);
     }
 }
