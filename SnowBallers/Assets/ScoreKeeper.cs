9f273149aba62d2e7331d7e9108219fa632e644f
brianpark24 <55064916+brianpark24@users.noreply.github.com>
2023-02-01 16:55:35 -0800

    end game scoreboard

--- SnowBallers/Assets/ScoreKeeper.cs
+++ SnowBallers/Assets/ScoreKeeper.cs
@@ -1,12 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.Linq;
 
 public class ScoreKeeper : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     private int score;
+    private ScoreData sd;
+
+    void Awake()
+    {
+        sd = new ScoreData();
+    }
+
+    public IEnumerable<Score> GetHighScores()
+    {
+        return sd.scores.OrderByDescending(x => x.score);
+    }
+
+    public void AddScore(Score score)
+    {
+        sd.scores.Add(score);
+    }
 
     // Start is called before the first frame update
     void Start()
