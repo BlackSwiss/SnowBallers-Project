b8d5e8fa52513454175476fe0378cb60a1c60784
AnthonySanchez150 <85590234+AnthonySanchez150@users.noreply.github.com>
2023-02-02 20:34:04 -0800

    Added sound effects for when a player gets hit

--- SnowBallers/Assets/Snowball.cs
+++ SnowBallers/Assets/Snowball.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Snowball : MonoBehaviour
 {
+    public AudioSource bonk;
+    public AudioSource dizzy;
     void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.tag == "Player")
@@ -11,13 +13,16 @@ public class Snowball : MonoBehaviour
             Debug.Log("Player hit!");
             collision.gameObject.GetComponent<Health>().decreaseHealth(1);
             //if(collision.gameObject.GetComponent<ChangeColor>())
-            //collision.gameObject.GetComponent<ChangeColor>().ChangeObjectColor();
+            collision.gameObject.GetComponent<ChangeColor>().ChangeObjectColor();
             collision.gameObject.GetComponent<Health>().hitAnimation();
 
             foreach (Transform c in transform)
             {
                 c.gameObject.GetComponent<ParticleSystem>().Play();
             }
+
+            bonk.Play();
+            dizzy.Play();
 
         }
     }
