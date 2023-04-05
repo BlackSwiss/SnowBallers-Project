1db35d15e920d5f1ce46dff51fa736988f6e7467
BlackSwiss <47673343+BlackSwiss@users.noreply.github.com>
2023-04-04 19:44:13 -0700

    Sync'd snowball generation over server

--- SnowBallers/Assets/Scripts/XRGrabbableObject.cs
+++ SnowBallers/Assets/Scripts/XRGrabbableObject.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class XRGrabbableObject : XRBaseInteractable
 {
@@ -14,7 +16,7 @@ public class XRGrabbableObject : XRBaseInteractable
     protected override void OnSelectEntered(SelectEnterEventArgs args)
     {
         // Instantiate object
-        GameObject newObject = Instantiate(grabbableObject, transformToInstantiate.position, Quaternion.identity);
+        GameObject newObject = PhotonNetwork.Instantiate("Snowball", transformToInstantiate.position, Quaternion.identity);
 
         // Get grab interactable from prefab
         XRGrabInteractable objectInteractable = newObject.GetComponent<XRGrabInteractable>();
